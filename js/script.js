d9c8825cd6b1b02deb15292de5e0228716ba25a2
mountainboy01 <dharmendrachauhan977@gmail.com>
2024-07-21 19:08:02 +0530

    project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/************** NAVBAR ***********/
+const hamburger = document.querySelector("#hamburger");
+const closeIcon = document.querySelector("#close-icon");
+const menu = document.querySelector("#menu");
+const mobile = document.querySelector("#mobile");
+
+
+hamburger.addEventListener("click",()=>{
+      mobile.classList.add("active");
+});
+
+closeIcon.addEventListener("click",()=>{
+      mobile.classList.remove("active");
+});
